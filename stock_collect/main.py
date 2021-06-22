f28e0dfe8be5e59f0877a03455e81817cb07a223
Eileen <lai.eling22@gmail.com>
2021-06-22 23:20:13 +0800

    ✨ upload data to firebase

--- stock_collect/main.py
+++ stock_collect/main.py
@@ -9,6 +9,18 @@ from stock_collect.message import Message
 from .gmail_service import GmailService
 
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("stock-collect-firebase-adminsdk-towjq-475cc7a29e.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+dest = {"name": "Eileen", "state": "Taipie", "country": "Taiwan"}
+db.collection("trade_data").add(dest)
+
+
 def crawl_data(param):
     id = param["id"]
     progress: Progress = param["progress"]
@@ -25,22 +37,23 @@ def crawl_data(param):
 
 def main():
     # the search string for only show the email with the string
-    QUERY_STRING = "玉山證券經紀本部"
+    # QUERY_STRING = "玉山證券經紀本部"
 
-    gmail = GmailService()
-    msg_list = gmail.get_all_message_id_list(q=QUERY_STRING)
-    print(f"crawl {len(msg_list)} files")
+    # gmail = GmailService()
+    # msg_list = gmail.get_all_message_id_list(q=QUERY_STRING)
+    # print(f"crawl {len(msg_list)} files")
 
-    with Progress() as progress:
-        lock = Lock()
-        task = progress.add_task("[red]crawl...", total=len(msg_list))
-        data = [
-            {"lock": lock, "progress": progress, "task": task, "id": msg["id"]}
-            for msg in msg_list
-        ]
+    # with Progress() as progress:
+    #     lock = Lock()
+    #     task = progress.add_task("[red]crawl...", total=len(msg_list))
+    #     data = [
+    #         {"lock": lock, "progress": progress, "task": task, "id": msg["id"]}
+    #         for msg in msg_list
+    #     ]
 
-        with ThreadPool(5) as p:
-            p.map(crawl_data, data)
+    #     with ThreadPool(5) as p:
+    #         p.map(crawl_data, data)
+    print("good")
 
 
 if __name__ == "__main__":

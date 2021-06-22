5a011885ca4e8c9baf881e49dc5a4c67a4a2bb13
Eileen <lai.eling22@gmail.com>
2021-06-22 11:45:40 +0800

    ➕ add pandsa to deal with xlsx file

--- stock_collect/main.py
+++ stock_collect/main.py
@@ -1,46 +1,19 @@
-from multiprocessing import Lock
-from multiprocessing.pool import ThreadPool
-
-from rich import print
-from rich.progress import Progress
-
-from stock_collect.message import Message
-
-from .gmail_service import GmailService
-
-
-def crawl_data(param):
-    id = param["id"]
-    progress: Progress = param["progress"]
-    task = param["task"]
-    lock = param["lock"]
-
-    msg = Message(id)
-    msg.crawl_data()
-
-    lock.acquire()
-    progress.update(task, advance=1)
-    lock.release()
+from stock_collect.message import crawl_excel_files
+import pandas
+import warnings
 
 
 def main():
     # the search string for only show the email with the string
     QUERY_STRING = "fugletrade 交易明細"
-
-    gmail = GmailService()
-    msg_list = gmail.get_all_message_id_list(q=QUERY_STRING)
-    print(f"crawl {len(msg_list)} files")
-
-    with Progress() as progress:
-        lock = Lock()
-        task = progress.add_task("[red]crawl...", total=len(msg_list))
-        data = [
-            {"lock": lock, "progress": progress, "task": task, "id": msg["id"]}
-            for msg in msg_list
-        ]
-
-        with ThreadPool(5) as p:
-            p.map(crawl_data, data)
+    # crawl_excel_files(QUERY_STRING)
+    
+    warnings.simplefilter("ignore")
+    x = pandas.read_excel("att_files\\交易明細20190125.xlsx",sheet_name='交易明細')
+    warnings.simplefilter("default")
+    
+    print(x)
+    
 
 
 if __name__ == "__main__":
--- stock_collect/message.py
+++ stock_collect/message.py
@@ -1,5 +1,9 @@
 import base64
 import os
+from multiprocessing import Lock
+from multiprocessing.pool import ThreadPool
+
+from rich.progress import Progress
 
 from stock_collect.gmail_service import GmailService
 
@@ -47,3 +51,34 @@ class Message:
             with open(file_name, "wb") as f:
                 f.write(file_data)
         # print(f"add {path}...", end="\r")
+
+
+def crawl_data(param):
+    id = param["id"]
+    progress: Progress = param["progress"]
+    task = param["task"]
+    lock = param["lock"]
+
+    msg = Message(id)
+    msg.crawl_data()
+
+    lock.acquire()
+    progress.update(task, advance=1)
+    lock.release()
+
+
+def crawl_excel_files(query_str):
+    gmail = GmailService()
+    msg_list = gmail.get_all_message_id_list(q=query_str)
+    print(f"crawl {len(msg_list)} files")
+
+    with Progress() as progress:
+        lock = Lock()
+        task = progress.add_task("[red]crawl...", total=len(msg_list))
+        data = [
+            {"lock": lock, "progress": progress, "task": task, "id": msg["id"]}
+            for msg in msg_list
+        ]
+
+        with ThreadPool(5) as p:
+            p.map(crawl_data, data)

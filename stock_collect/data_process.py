d18ffea01cc0d266d09eea7095116c90be15c9b9
Eileen <lai.eling22@gmail.com>
2021-06-26 02:53:23 +0800

    ✨ turn excel data to json format

---
+++ stock_collect/data_process.py
@@ -0,0 +1,57 @@
+import json
+import os
+import warnings
+from os.path import join
+from typing import Any
+
+import pandas as pd  # type: ignore
+from pandas.core.frame import DataFrame  # type: ignore
+
+
+def get_dataframe_from_excel(save_folder: str, save_excel: bool = False) -> DataFrame:
+    df_list = []
+    warnings.simplefilter("ignore")
+    for f in os.listdir(save_folder):
+        path = join(save_folder, f)
+        df = pd.read_excel(path, sheet_name="交易明細", converters={"代碼": str})
+        df_list.append(df)
+    warnings.simplefilter("default")
+    df = pd.concat(df_list).fillna(0)
+
+    if save_excel:
+        with pd.ExcelWriter("trade_data.xlsx") as writer:
+            df.to_excel(writer, index=False)
+    return df
+
+
+def filter_for_database(df: DataFrame) -> DataFrame:
+    df = df.reset_index(drop=True)
+    target_columns_dict = {
+        "交易別": "trade_type",
+        "代碼": "code",
+        "商品名稱": "ch_name",
+        "成交股數": "num",
+        "成交單價": "price",
+        "手續費": "fee",
+        "交易稅": "tax",
+        "交易日期": "date",
+    }
+    target_columns_list = [en_name for ch_name, en_name in target_columns_dict.items()]
+    df = df.rename(columns=target_columns_dict)
+    df = df.replace("現買", "buy")
+    df = df.replace("現賣", "sell")
+    df = df.filter(items=target_columns_list)
+    return df
+
+
+def dump_json(df: DataFrame) -> Any:
+    result = df.to_json(orient="index", force_ascii=False)
+    json_data = json.loads(result)
+    return json_data
+
+
+def get_json_from_excel(save_folder: str, save_excel: bool = False) -> Any:
+    df = get_dataframe_from_excel(save_folder, save_excel)
+    df = filter_for_database(df)
+    json_data = dump_json(df[:5])
+    return json_data
--- stock_collect/main.py
+++ stock_collect/main.py
@@ -1,30 +1,9 @@
-import os
-import warnings
-from os.path import join
-
 import firebase_admin  # type: ignore
-import pandas as pd  # type: ignore
 from firebase_admin import credentials, firestore
-from pandas.core.frame import DataFrame  # type: ignore
-
-from .message import crawl_excel_files
 from rich import print
 
-
-def get_trade_data(save_folder: str, save_excel: bool = False) -> DataFrame:
-    df_list = []
-    warnings.simplefilter("ignore")
-    for f in os.listdir(save_folder):
-        path = join(save_folder, f)
-        df = pd.read_excel(path, sheet_name="交易明細", converters={"代碼": str})
-        df_list.append(df)
-    warnings.simplefilter("default")
-    df = pd.concat(df_list).fillna(0)
-
-    if save_excel:
-        with pd.ExcelWriter("trade_data.xlsx") as writer:
-            df.to_excel(writer, index=False)
-    return df
+from .data_process import get_json_from_excel
+from .message import crawl_excel_files
 
 
 def main():
@@ -33,31 +12,10 @@ def main():
     SAVE_FOLDER = "att_files"
     # crawl_excel_files(QUERY_STRING, SAVE_FOLDER)
 
-    df = get_trade_data(SAVE_FOLDER)
-    print(df)
-    df = df.reset_index(drop=True)
-    target_columns_dict = {
-        "交易別": "trade_type",
-        "代碼": "code",
-        "商品名稱": "ch_name",
-        "成交股數": "num",
-        "成交單價": "price",
-        "手續費": "fee",
-        "交易稅": "tax",
-        "交易日期": "date",
-    }
-    target_columns_list = [en_name for ch_name, en_name in target_columns_dict.items()]
-    print(target_columns_list)
-    df = df.rename(columns=target_columns_dict)
-    df = df.replace("現買", "buy")
-    df = df.replace("現賣", "sell")
-    df = df.filter(items=target_columns_list)
-    print(df)
-    # print(df.to_json(orient="index"))
+    data = get_json_from_excel(SAVE_FOLDER)
+    print(data["0"])
 
-    # cred = credentials.Certificate(
-    #     "stock-collect-firebase-adminsdk.json"
-    # )
+    # cred = credentials.Certificate("stock-collect-firebase-adminsdk.json")
     # firebase_admin.initialize_app(cred)
 
     # db = firestore.client()

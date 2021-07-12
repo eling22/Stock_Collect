9b4ce26a3ecdb436d0dd10f5e706e9db865b6cea
Eileen <lai.eling22@gmail.com>
2021-07-12 14:51:33 +0800

    ✅ add view test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
+from typing import DefaultDict
+from collections import defaultdict
 from stock_collect.database.database import DataBase
-from stock_collect.fetch_trade_data.data_process import (dump_json,
-                                                         get_df_from_excel)
+from stock_collect.fetch_trade_data.data_process import dump_json, get_df_from_excel
 from stock_collect.fetch_trade_data.message import crawl_excel_files
+import matplotlib.pylab as plt
+from rich import print
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter, AutoMinorLocator
+import datetime as dt
 
 
 def test_crawl_data():
@@ -10,14 +16,35 @@ def test_crawl_data():
     crawl_excel_files(QUERY_STRING, SAVE_FOLDER)
 
 
-def test_main():
+def test_draw_trade_data_view():
     # the search string for only show the email with the string
     SAVE_FOLDER = "att_files"
     USER_UID = "z1KBIEE1QFXYzB2lE6f5hI9ArfR2"
 
     df = get_df_from_excel(SAVE_FOLDER)
-    df.to_csv("trade_data.csv")
-    data = dump_json(df)
+    # df.to_csv("trade_data.csv")
+    print(df)
+    data = defaultdict(int)
+    money = 0
+    for x in df.values:
+        [trade_type, code, ch_name, num, price, fee, tax, date] = x
+        money -= fee
+        if trade_type is "buy":
+            money -= num * price
+        else:  # sell
+            money += num * price
+            money -= tax
+        date = dt.datetime.strptime(str(date), "%Y%m%d").date()
+        data[date] = money
 
-    db = DataBase(USER_UID)
-    db.add_trade_data(data)
+    lists = sorted(data.items())
+    x, y = zip(*lists)
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y/%m/%d"))
+    plt.gca().yaxis.set_major_formatter(FormatStrFormatter("%d"))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=90))
+    plt.plot(x, y)
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+    print(data)

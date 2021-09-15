be78d7fd7af29352eb46df77133140d19c3d379a
Eileen <lai.eling22@gmail.com>
2021-09-15 23:01:12 +0800

    ✨ calculate the num of each div & split data

--- tests/test_dividends.py
+++ tests/test_dividends.py
@@ -6,6 +6,8 @@ import yfinance as yf
 from datetime import date, datetime
 from rich import print
 import datetime as dt
+import math
+import numpy as np
 
 
 def str_to_datatime(time: str, format: str = "%Y%m%d") -> date:
@@ -15,9 +17,47 @@ def str_to_datatime(time: str, format: str = "%Y%m%d") -> date:
 def datatime_to_str(time: date, format: str = "%Y%m%d") -> str:
     return time.strftime(format)
 
+    df["ch_name"] = ch_name
+
+
+def stock_property_to_df(
+    trade_type: str, prop: Series, stock_id: str, ch_name: str
+) -> DataFrame:
+    df: DataFrame = prop.to_frame()
+    df = df.reset_index(level=["Date"])
+    df["Date"] = df["Date"].dt.strftime("%Y%m%d").astype(int)
+    df = df.rename(columns={"Date": "date"})
+    df["trade_type"] = trade_type
+    df["code"] = stock_id
+    df["ch_name"] = ch_name
+    return df
+
+
+def calculate_div_split(df: DataFrame) -> DataFrame:
+    stock_num = 0
+    split_date = 0
+    split_num = 0
+    for idx, row in df.iterrows():
+        if split_date < row["date"]:
+            stock_num += split_num
+            split_num = 0
+        if row["trade_type"] == "buy":
+            stock_num += row["num"]
+        elif row["trade_type"] == "sell":
+            stock_num -= row["num"]
+        elif row["trade_type"] == "dividend":
+            df.at[idx, "num"] = stock_num
+            df.at[idx, "price"] = row["dividend"]
+        elif row["trade_type"] == "split":
+            split_num = math.floor(stock_num * (row["split"] - 1))
+            df.at[idx, "num"] = split_num
+            split_date = row["date"]
+    return df
+
 
 def test_div():
-    stock_id = "2330"
+    # stock_id = "2330"
+    stock_id = "2884"
     stock_info = yf.Ticker(f"{stock_id}.TW")
     USER_UID = "z1KBIEE1QFXYzB2lE6f5hI9ArfR2"
     db = DataBase(USER_UID)
@@ -25,28 +65,29 @@ def test_div():
     # get my trade data
     trade_data_df = db.get_trade_data_by_stock_id(stock_id)
     ch_name = trade_data_df["ch_name"].iloc[0]
-    print(trade_data_df)
+    # print(trade_data_df)
 
-    # get stock dividend(div) data
+    # get stock data
     start_date = str_to_datatime(trade_data_df["date"].iloc[0])
     end_data = datetime.now()  # end_date will not be counted
     df = stock_info.history(start=start_date, end=end_data)
-    print(df)
+    # print(df)
 
-    # turn my div data to my trade data format
-    div_df: DataFrame = stock_info.dividends.to_frame()
-    div_df = div_df.reset_index(level=["Date"])
-    div_df["Date"] = div_df["Date"].dt.strftime("%Y%m%d")
-    div_df = div_df.rename(columns={"Date": "date", "Dividends": "price"})
-    div_df["trade_type"] = "dividend"
-    div_df["code"] = stock_id
-    div_df["ch_name"] = ch_name
-    print(div_df)
+    # turn dividend(div) & split data to my trade data format
+    div_df = stock_property_to_df("dividend", stock_info.dividends, stock_id, ch_name)
+    split_df = stock_property_to_df("split", stock_info.splits, stock_id, ch_name)
 
-    # put div data to my trade data
-    trade_data_df = trade_data_df.append(div_df).fillna(0)
+    # put div & split data to my trade data
+    trade_data_df = trade_data_df.append(div_df).append(split_df).fillna(0)
+    trade_data_df = trade_data_df.sort_values(by=["date"])
+    trade_data_df = trade_data_df.reset_index(drop=True)
+    trade_data_df = trade_data_df.rename(
+        columns={"Stock Splits": "split", "Dividends": "dividend"}
+    )
     print(trade_data_df)
 
-    # calculate the num of each div data
+    # calculate the num of each div & split data
+    trade_data_df = calculate_div_split(trade_data_df)
+    print(trade_data_df)
 
     # put the div data to database
